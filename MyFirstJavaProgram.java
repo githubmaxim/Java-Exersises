7e6daf31a0173e0c41a491da968e7c83265806ea
githubmaxim <42032840+githubmaxim@users.noreply.github.com>
2018-08-02 14:33:56 +0300

    MyFirstJavaProgram

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+
+public class MyFirstJavaProgram
+{
+  public static void main (String []args)
+  {
+    /*
+      Длинный комент
+    */ 
+    System.out.println("Привет мир!"); //в строчку Короткий комент
+   }
+}
